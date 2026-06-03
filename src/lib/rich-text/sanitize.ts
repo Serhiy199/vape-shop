bd523db7cdd8ac0b84e1accaf50fa8b2075c119a
Serhiy Horodetskiy <bryklin.best.1994@gmail.com>
2026-06-03 13:46:55 +0300

    виправив баг

--- src/lib/rich-text/sanitize.ts
+++ src/lib/rich-text/sanitize.ts
@@ -1,56 +1,62 @@
-import DOMPurify from "isomorphic-dompurify";
+import sanitizeHtml from "sanitize-html";
 
-const SAFE_URI_PATTERN =
-  /^(?:(?:https?|mailto|tel):|[^a-z]|[a-z+.-]+(?:[^a-z+.-:]|$))/i;
+const allowedTags = [
+  "a",
+  "blockquote",
+  "br",
+  "div",
+  "em",
+  "h2",
+  "h3",
+  "h4",
+  "hr",
+  "img",
+  "li",
+  "ol",
+  "p",
+  "s",
+  "span",
+  "strong",
+  "table",
+  "tbody",
+  "td",
+  "th",
+  "thead",
+  "tr",
+  "u",
+  "ul",
+];
 
 export function sanitizeRichTextHtml(html: string | null | undefined) {
   if (!html?.trim()) {
     return "";
   }
 
-  return DOMPurify.sanitize(html, {
-    ALLOWED_ATTR: [
-      "alt",
-      "class",
-      "colspan",
-      "href",
-      "rel",
-      "rowspan",
-      "src",
-      "style",
-      "target",
-      "title",
-      "width",
-    ],
-    ALLOWED_TAGS: [
-      "a",
-      "blockquote",
-      "br",
-      "div",
-      "em",
-      "h2",
-      "h3",
-      "h4",
-      "hr",
-      "img",
-      "li",
-      "ol",
-      "p",
-      "s",
-      "span",
-      "strong",
-      "table",
-      "tbody",
-      "td",
-      "th",
-      "thead",
-      "tr",
-      "u",
-      "ul",
-    ],
-    ALLOWED_URI_REGEXP: SAFE_URI_PATTERN,
-    ADD_ATTR: ["target"],
-    FORBID_ATTR: ["onerror", "onload", "onclick", "onmouseover"],
-    FORBID_TAGS: ["iframe", "object", "script", "style"],
+  return sanitizeHtml(html, {
+    allowedAttributes: {
+      a: ["href", "rel", "target", "title"],
+      img: ["alt", "src", "title", "width", "style"],
+      "*": ["class", "colspan", "rowspan", "style"],
+    },
+    allowedSchemes: ["http", "https", "mailto", "tel"],
+    allowedSchemesByTag: {
+      img: ["http", "https"],
+    },
+    allowedStyles: {
+      "*": {
+        "text-align": [/^left$/, /^center$/, /^right$/],
+      },
+      img: {
+        height: [/^auto$/],
+        width: [/^\d+%$/, /^\d+px$/],
+      },
+    },
+    allowedTags,
+    transformTags: {
+      a: sanitizeHtml.simpleTransform("a", {
+        rel: "noopener noreferrer",
+        target: "_blank",
+      }),
+    },
   });
 }
